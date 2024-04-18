c7336e6a25b0595f20ea222619715a974a1983c4
Vinso <8vinso8@gmail.com>
2024-04-18 21:11:08 +1000

    waybar

--- configs/default.nix
+++ configs/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./hypr
     ./alacritty
+    ./waybar
   ];
 }
--- configs/waybar/default.nix
+++ configs/waybar/default.nix
@@ -5,46 +5,48 @@
     enable = true;
     style = ./style.css;
     settings = {
-      layer = "top";
-      margin-top = 5;
-      margin-left = 5;
-      margin-right = 5;
-      margin-bottom = 5;
-      modules-left = [ "hyprland/workspaces" ];
-      modules-center= [];
-      modules-right= [
-          "tray"
-          "cpu"
-          "memory"
-          "pulseaudio"
-          "hyprland/language"
-          "clock"
-      ];
-      "tray" = {
-          spacing = 10;
+      mainBar = {
+        layer = "top";
+        margin-top = 5;
+        margin-left = 5;
+        margin-right = 5;
+        margin-bottom = 5;
+        modules-left = [ "hyprland/workspaces" ];
+        modules-center= [];
+        modules-right= [
+            "tray"
+            "cpu"
+            "memory"
+            "pulseaudio"
+            "hyprland/language"
+            "clock"
+        ];
+        "tray" = {
+            spacing = 10;
+        };
+        "clock"= {
+            format = "{:%d.%m.%y | %H:%M}";
+        };
+        "cpu"= {
+            format = "{usage}%";
+            tooltip = false;
+        };
+        "memory"= {
+            format = "{used}GiB|{swapUsed}GiB";
+            tooltip = false;
+        };
+        "pulseaudio"= {
+            format = "{format_source}";
+            format-source = "MIC";
+            format-source-muted = "<span foreground='#f38ba8'>MIC</span>";
+            on-scroll-up = "";
+            on-scroll-down = "";
+        };
+        "hyprland/language" = {
+            format-en= "EN";
+            format-ru= "RU";
+        };
       };
-      "clock"= {
-          format = "{=%d.%m.%y | %H=%M}";
-      };
-      "cpu"= {
-          format = "{usage}%";
-          tooltip = false;
-      };
-      "memory"= {
-          format = "{used}GiB|{swapUsed}GiB";
-          tooltip = false;
-      };
-      "pulseaudio"= {
-          format = "{format_source}";
-          format-source = "MIC";
-          format-source-muted = "<span foreground='#f38ba8'>MIC</span>";
-          on-scroll-up = "";
-          on-scroll-down = "";
-      };
-      "hyprland/language" = {
-          format-en= "EN";
-          format-ru= "RU;
-      }
     };
-  }
+  };
 }
